28792e650d68b3b1dbf6c316f4bc0555cdd3ae08
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-11-15 14:18:54 -0500

    added colors, information to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>See if your favorite song can win the battle of taste!</p>
+				<div className="song-search">
+					<input
+						type="text"
+						id="header-search"
+						placeholder="Search for your favorite song..."
+						name="s"
+					/>
+					<button type="submit">Search</button>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
