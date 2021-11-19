39893ffa40f769c8ba86236e611fed32b2844a1b
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-11-18 23:41:50 -0500

    completed the api route and mapping the filtered data

--- server/index.js
+++ server/index.js
@@ -1,60 +1,50 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const q = require("../helpers/querySpotify.js");
+const q = require('../helpers/querySpotify.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.send({ data: "peepeepoopoo check!" });
+app.get('/api', (req, res) => {
+	res.send({ data: 'peepeepoopoo check!' });
 });
 
-app.get("/api/song_search/:songName", async (req, res) => {
-  let data = req.params.songName;
-  console.log("THIS IS WHAT I AM SEARCHING FOR: " + data);
-  try {
-    const query = await q.querySpotify(data);
-    // const filteredData = query.tracks.items.map((item) => {
-    //   const trackName = item.name;
-    //   const artistName = item.album.artists[0].name;
-    //   const albumCover = item.images[1].url;
-    //   const releaseDate = item.release_date;
+app.get('/api/song_search/:songName', async (req, res) => {
+	let data = req.params.songName;
+	console.log('THIS IS WHAT I AM SEARCHING FOR: ' + data);
+	try {
+		const query = await q.querySpotify(data);
+		const filteredData = query.tracks.items.map((track) => {
+			const albumArt = {
+				smallAlbumArt: track.album.images[2],
+				mediumAlbumArt: track.album.images[1],
+				largeAlbumArt: track.album.images[0],
+			};
 
-    //   const responseObj = { trackName, artistName, albumCover, releaseDate };
-    //   return responseObj;
-    // });
+			const previewUrl = track.preview_url;
+			const trackName = track.name;
+			const albumName = track.album.name;
+			const artistName = track.album.artists[0].name;
+			const albumLink = track.album.artists[0].external_urls.spotify;
 
-    // console.log('This is the filteredData: ', filteredData)
-    console.log("This is the result of the query: ", query);
-    const firstItem = query.tracks.items[0];
-    console.log("THis is the first item: ", firstItem);
-    const smallAlbumArt = query.tracks.items[0].album.images[0];
-    const mediumAlbumArt = query.tracks.items[0].album.images[1];
-    const largeAlbumArt = query.tracks.items[0].album.images[2];
+			const resObj = {
+				trackName,
+				artistName,
+				albumName,
+				albumLink,
+				previewUrl,
+				albumArt,
+			};
 
-    const previewUrl = query.tracks.items[0].preview_url;
-    const extractedSongName = query.tracks.items[0].name;
-    const firstArtist = query.tracks.items[0].album.artists[0];
-    const artistExtracted = query.tracks.items[0].album.artists[0].name;
-    const albumLink =
-      query.tracks.items[0].album.artists[0].external_urls.spotify;
-
-    res.json({
-      previewUrl,
-      extractedSongName,
-      artistExtracted,
-      albumLink,
-      smallAlbumArt,
-      mediumAlbumArt,
-      largeAlbumArt,
-    });
-    // res.json(firstItem)
-  } catch (error) {
-    res.send(error);
-  }
+			return resObj;
+		});
+		res.json(filteredData);
+	} catch (error) {
+		res.send(error);
+	}
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
