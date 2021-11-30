57e54902af88c67e5c078076841f95ee17ce8c33
Muin Syed <mhc.msyed@gmail.com>
2021-11-29 20:25:59 -0500

    Cleared up Routes in the App.js file to simplify.

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,7 @@ function App() {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Hero />}>
-                    <Route index element={<Hero />} />
-
-                    {/* <Route path="dashboard" element={<Dashboard />} /> */}
-
-                    {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-                    <Route path="*" element={<div> No matching page</div>} />
-                </Route>
+                <Route path="/" element={<Hero />} />
                 <Route path="search-page" element={<SearchPage />} />
             </Routes>
         </div>
